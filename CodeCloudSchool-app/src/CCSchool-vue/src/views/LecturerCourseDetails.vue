6b789a02ce55a4a0b66de0f773614199f0545c87
mwape-k <82158230+mwape-k@users.noreply.github.com>
2025-05-26 21:50:50 +0200

    CORE(Pages): updating live update

--- CodeCloudSchool-app/src/CCSchool-vue/src/views/LecturerCourseDetails.vue
+++ CodeCloudSchool-app/src/CCSchool-vue/src/views/LecturerCourseDetails.vue
@@ -5,6 +5,9 @@ import CButton from '@/components/ui/CButton.vue'
 import Divider from 'primevue/divider'
 import CMarkBreakdown from '@/components/CMarkBreakdown.vue'
 import LecturerCard from '@/components/LecturerCard.vue'
+import CButtonIcon from '@/components/ui/Cbutton-icon.vue'
+import { PencilLine, Maximize2 } from 'lucide-vue-next'
+import { ref } from 'vue'
 
 const assessmentGroups = [
   {
@@ -35,11 +38,24 @@ const assessmentGroups = [
     ],
   },
 ]
+
+const isEditing = ref(false)
 </script>
 
 <template>
   <div class="announcemnts-section">
-    <h1 class="section-header">Announcements</h1>
+    <div class="cd-header">
+      <div class="right-cd">
+        <h1 class="section-header">Announcements</h1>
+      </div>
+      <div class="left-cd">
+        <CButtonIcon type="primary" size="md" btnIconLabel="View All Announcements">
+          <template #icon>
+            <Maximize2 size="16" />
+          </template>
+        </CButtonIcon>
+      </div>
+    </div>
     <div class="card-container">
       <CardComp
         cardType="announcement"
@@ -74,87 +90,121 @@ const assessmentGroups = [
   </div>
 
   <div class="course-details-section">
-    <h1 class="section-header-lg">Computer Science | Semester 1</h1>
+    <div class="course-section-header">
+      <div class="left-cs-header">
+        <h1 class="section-header-lg">Computer Science | Semester 1</h1>
+      </div>
+      <div class="right-cs-header">
+        <CButtonIcon
+          id="edit-course"
+          type="primary"
+          size="md"
+          btnIconLabel="Update Course Details"
+          @click="isEditing = true"
+        >
+          <template #icon>
+            <PencilLine size="20" />
+          </template>
+        </CButtonIcon>
+      </div>
+    </div>
+
     <div class="course-cover-img">
       <img src="https://picsum.photos/seed/picsum/1200/1600" alt="course cover img" />
     </div>
-  </div>
 
-  <div class="bio-section">
-    <h1 class="section-header">What You Will Learn</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, eum porro eius earum explicabo
-      mollitia cupiditate nemo veritatis sint enim unde, totam hic nobis placeat itaque asperiores
-      sed officia pariatur! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum adipisci
-      eum dolorem harum a accusamus quos! Delectus nihil officiis eos nostrum porro corrupti quia
-      tempore, obcaecati officia voluptatum quae ducimus?
-    </p>
-  </div>
-
-  <div class="sem-overview-section">
-    <CListGroup
-      :items="[
-        { header: 'Week 1 ', description: ' Course Introduction & Briefing.' },
-        { header: 'Week 2', description: 'Type-sensitive programming' },
-        { header: 'Week 3', description: ' Basic software architecture' },
-      ]"
-    />
-  </div>
-
-  <div class="semester-break">
-    <h3>SEMESTER BREAK</h3>
-  </div>
-
-  <div class="google-slides-section">
-    <h1 class="section-header">SEMESTER BRIEF</h1>
-    <div class="slides-container">
-      <iframe
-        src="https://docs.google.com/presentation/d/e/2PACX-1vRGtMnpYEVo6hbUXId7ZBzDVjzEbtUL6hu4qx8YRp8GLh_ITDH_cUTZSJgxnCh-ztxNWQdj4ozAmWem/pubembed?start=false&loop=true&delayms=3000"
-        frameborder="0"
-        width="960"
-        height="569"
-        allowfullscreen="true"
-        mozallowfullscreen="true"
-        webkitallowfullscreen="true"
-        class="slides-iframe"
-      ></iframe>
+    <div class="bio-section">
+      <div v-if="isEditing == false">
+        <h1 class="section-header">What You Will Learn</h1>
+      </div>
+      <div v-else-if="isEditing == true">
+        <div class="cd-header">
+          <div class="right-cd">
+            <h1 class="section-header">Announcements</h1>
+          </div>
+          <div class="left-cd">
+            <CButtonIcon type="primary" size="md" btnIconLabel="Edit Section">
+              <template #icon>
+                <PencilLine size="16" />
+              </template>
+            </CButtonIcon>
+          </div>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, eum porro eius earum
+        explicabo mollitia cupiditate nemo veritatis sint enim unde, totam hic nobis placeat itaque
+        asperiores sed officia pariatur! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Rerum adipisci eum dolorem harum a accusamus quos! Delectus nihil officiis eos nostrum porro
+        corrupti quia tempore, obcaecati officia voluptatum quae ducimus?
+      </p>
     </div>
-    <Divider class="mt-5 mb-5" />
-  </div>
 
-  <div class="mark-breakdown-section">
-    <h1 class="section-header">Mark Breakdown</h1>
-    <CButton type="secondary" size="md" :disabled="true" class="mark-breadown-btn mb-2"
-      >MARK BREKDOWN</CButton
-    >
-    <br />
-    <CMarkBreakdown :items="assessmentGroups" />
-    <Divider class="mb-2" />
-  </div>
+    <div class="sem-overview-section">
+      <CListGroup
+        :items="[
+          { header: 'Week 1 ', description: ' Course Introduction & Briefing.' },
+          { header: 'Week 2', description: 'Type-sensitive programming' },
+          { header: 'Week 3', description: ' Basic software architecture' },
+        ]"
+      />
+    </div>
 
-  <div class="year-breakdown-section">
-    <h1 class="section-header">Year Breakdown</h1>
-    <CButton type="secondary" size="md" :disabled="true" class="year-breadown-btn mb-2"
-      >Semester 1</CButton
-    >
+    <div class="semester-break">
+      <h3>SEMESTER BREAK</h3>
+    </div>
 
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quisquam assumenda soluta
-      repellendus saepe expedita, perspiciatis distinctio? Aut culpa vero placeat mollitia animi,
-      nesciunt, magnam non eos itaque perferendis dicta.
-    </p>
+    <div class="google-slides-section">
+      <h1 class="section-header">SEMESTER BRIEF</h1>
+      <div class="slides-container">
+        <iframe
+          src="https://docs.google.com/presentation/d/e/2PACX-1vRGtMnpYEVo6hbUXId7ZBzDVjzEbtUL6hu4qx8YRp8GLh_ITDH_cUTZSJgxnCh-ztxNWQdj4ozAmWem/pubembed?start=false&loop=true&delayms=3000"
+          frameborder="0"
+          width="960"
+          height="569"
+          allowfullscreen="true"
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+          class="slides-iframe"
+        ></iframe>
+      </div>
+      <Divider class="mt-5 mb-5" />
+    </div>
 
-    <Divider class="year-break-div" />
+    <div class="mark-breakdown-section">
+      <h1 class="section-header">Mark Breakdown</h1>
+      <CButton type="secondary" size="md" :disabled="true" class="mark-breadown-btn mb-2"
+        >MARK BREKDOWN</CButton
+      >
+      <br />
+      <CMarkBreakdown :items="assessmentGroups" />
+      <Divider class="mb-2" />
+    </div>
 
-    <CButton type="secondary" size="md" :disabled="true" class="year-breadown-btn"
-      >Semester 2</CButton
-    >
+    <div class="year-breakdown-section">
+      <h1 class="section-header">Year Breakdown</h1>
+      <CButton type="secondary" size="md" :disabled="true" class="year-breadown-btn mb-2"
+        >Semester 1</CButton
+      >
 
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quisquam assumenda soluta
-      repellendus saepe expedita, perspiciatis distinctio? Aut culpa vero placeat mollitia animi,
-      nesciunt, magnam non eos itaque perferendis dicta.
-    </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quisquam assumenda
+        soluta repellendus saepe expedita, perspiciatis distinctio? Aut culpa vero placeat mollitia
+        animi, nesciunt, magnam non eos itaque perferendis dicta.
+      </p>
+
+      <Divider class="year-break-div" />
+
+      <CButton type="secondary" size="md" :disabled="true" class="year-breadown-btn"
+        >Semester 2</CButton
+      >
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quisquam assumenda
+        soluta repellendus saepe expedita, perspiciatis distinctio? Aut culpa vero placeat mollitia
+        animi, nesciunt, magnam non eos itaque perferendis dicta.
+      </p>
+    </div>
   </div>
 
   <div class="lecturer-contact-card-section">
@@ -179,6 +229,56 @@ const assessmentGroups = [
 
 .course-details-section {
   margin-bottom: 2.5rem;
+}
+
+/* ANNOUNCEMENTS */
+.cd-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.left-cd > .CButtonIcon {
+  width: 15rem;
+  margin-top: 5rem;
+}
+
+.announcemnts-section {
+  margin-bottom: 2rem;
+}
+
+.card-container {
+  background-color: #f5f5f5;
+  border-radius: 25px;
+  padding: 1rem;
+}
+
+.dashboard-view {
+  padding: 2rem;
+}
+
+.divider,
+.divider-card {
+  height: 1px;
+  background-color: #ccc;
+  margin: 1rem 0;
+}
+
+.divider-card {
+  width: 97%;
+  margin-left: 1.5%;
+}
+
+/*COURSE MASTER SECTION */
+.course-cover-img > img {
+  width: 100%;
+  height: 300px;
+  border-radius: 30px;
+  object-fit: cover;
+}
+
+.course-cover-img {
+  margin-bottom: 2rem;
 }
 
 .bio-section {
@@ -226,38 +326,19 @@ const assessmentGroups = [
   margin-bottom: 2rem;
 }
 
-/* ANNOUNCEMENTS */
-.announcemnts-section {
-  margin-bottom: 2rem;
+/* COURSE DETAILS SECTION */
+.course-details-section {
+  margin: 2.5rem;
+}
+.course-section-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
-.card-container {
-  background-color: #f5f5f5;
-  border-radius: 25px;
-  padding: 1rem;
-}
-
-.dashboard-view {
-  padding: 2rem;
-}
-
-.divider,
-.divider-card {
-  height: 1px;
-  background-color: #ccc;
-  margin: 1rem 0;
-}
-
-.divider-card {
-  width: 97%;
-  margin-left: 1.5%;
-}
-
-/*COURSE MASTER SECTION */
-.course-cover-img > img {
-  width: 100%;
-  height: 300px;
-  border-radius: 30px;
-  object-fit: cover;
+#edit-course {
+  width: 15rem;
+  margin-top: 5rem;
+  margin-bottom: 1rem;
 }
 </style>
