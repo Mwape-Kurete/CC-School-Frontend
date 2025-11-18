4fe44d7561ca24aabdbc710305c83a9608aaae26
mwape-k <82158230+mwape-k@users.noreply.github.com>
2025-11-18 15:45:47 +0200

    refactor on styling

--- CodeCloudSchool-app/src/CCSchool-vue/src/main.ts
+++ CodeCloudSchool-app/src/CCSchool-vue/src/main.ts
@@ -7,6 +7,10 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 
+import 'primevue/resources/themes/saga-blue/theme.css' // theme
+import 'primevue/resources/primevue.min.css' // core css
+import 'primeicons/primeicons.css' // icons
+
 import 'primeflex/primeflex.css' // primeflex (optional)
 import ToastService from 'primevue/toastservice'
 import Toast from 'primevue/toast'
