45e6a0bcbc0d2bc8ad16b04c7c1b15c6511ad913
mwape-k <82158230+mwape-k@users.noreply.github.com>
2025-11-18 15:38:30 +0200

    FIX: fix: remove primevue css imports completely -> deployment

--- CodeCloudSchool-app/src/CCSchool-vue/src/main.ts
+++ CodeCloudSchool-app/src/CCSchool-vue/src/main.ts
@@ -7,12 +7,6 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 
-//commenting the below out for deployment
-// import 'primevue/resources/themes/saga-blue/theme.css' // theme
-// import 'primevue/resources/primevue.min.css' // core css
-// import 'primeicons/primeicons.css' // icons
-
-//----------------------------------------------------
 import 'primeflex/primeflex.css' // primeflex (optional)
 import ToastService from 'primevue/toastservice'
 import Toast from 'primevue/toast'
